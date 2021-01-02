03ea9949c58bb110b3bd54655677bd7d7f73137b
Filipedgeraldo <filipedgeraldo@gmail.com>
2021-01-02 15:33:00 +0000

    fpga working!

--- hardware/fpga/vivado/ARTIX-7/iob_knn_stub.v
+++ hardware/fpga/vivado/ARTIX-7/iob_knn_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
-// Date        : Wed Dec  2 13:59:14 2020
+// Date        : Sat Jan  2 15:32:08 2021
 // Host        : FilipeLinux running 64-bit Ubuntu 20.04.1 LTS
 // Command     : write_verilog -force -mode synth_stub iob_knn_stub.v
 // Design      : iob_knn
